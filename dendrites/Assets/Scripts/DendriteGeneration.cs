8ac8e6f3a01e6e68bcf72117828b70d75d28bb4e
nothke <nothke@yahoo.com>
2014-05-31 20:19:41 +0200

    Start
    Started the project

---
+++ dendrites/Assets/Scripts/DendriteGeneration.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DendriteGeneration : MonoBehaviour
+{
+
+	public float forceFactor; // Random force multiplier
+	public float randomPercent; // This is the rate of spawning in percent per frame
+	public int numberOfTwins; // A maximum number of instances spawned in a single birth
+	public int maxBirths; // A maximum number of births an object can have
+	public int fertilityLoss; // After how many generations do they become infertile. This is used to limit the infinite generation
+
+	private int i;
+
+	void Update ()
+	{
+		rigidbody.AddForce (Random.Range (-1f, 1f) * forceFactor, Random.Range (-1f, 1f) * forceFactor, Random.Range (-1f, 1f) * forceFactor);
+	
+		if (Random.value < (0.01 * randomPercent)) {
+			if (i < maxBirths) {
+				for (int j = 0; j < numberOfTwins; j++) { // I previously used: "Random.Range (0, numberOfTwins)", but it was hard to control fps
+					GameObject newborn = Instantiate (gameObject, transform.position, Quaternion.identity) as GameObject;
+					newborn.GetComponent<DendriteGeneration>().randomPercent -= randomPercent / fertilityLoss;
+				}
+				i++;
+			}
+		}
+	}
+}
---
+++ dendrites/Assets/Scripts/Utility_Restart.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Utility_Restart : MonoBehaviour {
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Tab))
+			Application.LoadLevel (Application.loadedLevel);
+		if (Input.GetKeyDown (KeyCode.Escape))
+			Application.Quit ();
+	}
+}
